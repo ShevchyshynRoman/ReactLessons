c575f581f4553f995cbf5dfd6d80ac12f1ba725c
RomanShevchyshyn <romanshevchyshyn1998@gmail.com>
2021-05-17 21:23:14 +0300

    lesson 021 verstka dialogs.jsx + dialogs.module.css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,19 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-          Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogs_items}>
+                <div className={styles.dialog}>Misha</div>
+                <div className={styles.dialog + ' ' + styles.active_message}>Roman</div>
+                <div className={styles.dialog}>Danilo</div>
+                <div className={styles.dialog}>Anton</div>
+                <div className={styles.dialog}>Vasyl</div>
+            </div>
+            <div className={styles.dialogs_messages}>
+                <div className={styles.message}>Hi!</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}>You are welcome!</div>
+            </div>
         </div>
     )
 }
