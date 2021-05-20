8c82adb852eb649d4db977d569abc3882b96b808
RomanShevchyshyn <romanshevchyshyn1998@gmail.com>
2021-05-20 10:58:57 +0300

    lesson 022 (Route exact) add NavLink to Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,30 @@
 import styles from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogs_items}>
-                <div className={styles.dialog}>Misha</div>
-                <div className={styles.dialog + ' ' + styles.active_message}>Roman</div>
-                <div className={styles.dialog}>Danilo</div>
-                <div className={styles.dialog}>Anton</div>
-                <div className={styles.dialog}>Vasyl</div>
+                <div>
+                    <NavLink to='/dialogs/1'>Misha</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/dialogs/2'>Roman</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/dialogs/3'>Danilo</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/dialogs/4'>Anton</NavLink>
+                </div>
+                <div>
+                    <NavLink to='/dialogs/5'>Vasyl</NavLink>
+                </div>
             </div>
             <div className={styles.dialogs_messages}>
-                <div className={styles.message}>Hi!</div>
-                <div className={styles.message}>How are you?</div>
-                <div className={styles.message}>You are welcome!</div>
+                <div>Hi!</div>
+                <div>How are you?</div>
+                <div>You are welcome!</div>
             </div>
         </div>
     )
