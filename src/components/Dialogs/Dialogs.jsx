18920be09e34ee463fd112b3ac951e1e56e88f3a
RomanShevchyshyn <romanshevchyshyn1998@gmail.com>
2021-05-30 20:56:45 +0300

    lesson 031 homework

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,19 @@
 import styles from './Dialogs.module.css'
+import React from 'react'
 import DialogItem from "./DialogItem/DialogItem";
 import MessageItem from "./MessageItem/MessageItem";
-
-
 
 
 const Dialogs = (props) => {
     let DialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>) //мапимо массив DialogsData для відмаьовування <DialogItem/>
     let MessagesElements = props.state.messages.map(m => <MessageItem message={m.message} id={m.id}/>) //мапимо массив MessagesData для відмаьовування <MessageItem/>
+
+    let newMessageText = React.createRef();
+    let addMessage = () => {
+        let message = newMessageText.current.value;
+        alert(message)
+    }
+
 
     return (
         <div className={styles.dialogs}>
@@ -16,6 +22,14 @@ const Dialogs = (props) => {
             </div>
             <div className={styles.dialogs_messages}>
                 {MessagesElements} {/*Відмальовуємо MessageItem*/}
+                <div>
+                    <div>
+                        <textarea ref={newMessageText}></textarea>
+                    </div>
+                    <div>
+                        <button onClick={addMessage}>Add Message</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
