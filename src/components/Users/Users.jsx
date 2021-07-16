4e71c9675433f06a4f17f8a28695cc98c6b3a2d6
RomanShevchyshyn <romanshevchyshyn1998@gmail.com>
2021-07-16 18:55:00 +0300

    lesson 054 (life cycle methods, componentDidMount)

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from "axios";
+import userPhoto from "../../assets/user.png";
+import styles from "./users.module.css";
+
+class Users extends React.Component {
+
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
+                             alt="userPhoto"/>
+                    </div>
+                    <div>
+                        {
+                            u.followed
+                                ? <button onClick={() => this.props.unFollow(u.id)}>Follow</button>
+                                : <button onClick={() => this.props.follow(u.id)}>Unfollow</button>
+                        }
+                    </div>
+                </span>
+                        <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                         <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
+                    </span>
+                </span>
+                    </div>)
+                }
+            </div>
+        )
+    }
+}
+
+export default Users;
