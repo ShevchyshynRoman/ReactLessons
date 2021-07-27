5b4b9cf8f9d1e212ab3afdaedda8c4ebc64ec35f
RomanShevchyshyn <romanshevchyshyn1998@gmail.com>
2021-07-27 15:34:57 +0300

    lesson 061 (cookie, login в теории, auth/me)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {Route} from "react-router-dom"
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
@@ -8,13 +7,14 @@ import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 
 const App = (props) => {
     return (
         <div className='app-wrapper'>
-            <Header/>
+            <HeaderContainer/>
             <Navbar/>
             <div className='app-wrapper-content'>
                 <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import styles from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={styles.header}>
             <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
                 alt="Logo"/>
+            <div className={styles.loginBlock}>
+                {props.isAuth
+                    ? props.login
+                    : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     )
 }
